36ae1ab6116e6d24f9ecd7c58dfda43d76911aa8
Martin Ruthaivilavan <mmcruthai@gmail.com>
2023-03-06 15:12:24 -0600

    gunicorn addition

--- app/__init__.py
+++ app/__init__.py
@@ -6,28 +6,38 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 
 
-app = Flask(__name__)
-CORS(app)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
 
 
-login.login_view = '/login'
-login.login_message = 'Please log in to access this page.'
+def create_app():
+    app = Flask(__name__) 
 
-from app.blueprints.auth import bp as auth_bp
-app.register_blueprint(auth_bp)
+    CORS(app)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
 
-from app.blueprints.main import bp as main_bp
-app.register_blueprint(main_bp)
 
-from app.blueprints.social import bp as social_bp
-app.register_blueprint(social_bp)
+    login.login_view = '/login'
+    login.login_message = 'Please log in to access this page.'
 
-from app.blueprints.api import bp as api_bp
-app.register_blueprint(api_bp)
+    from app.blueprints.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+
+    from app.blueprints.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from app.blueprints.social import bp as social_bp
+    app.register_blueprint(social_bp)
+
+    from app.blueprints.api import bp as api_bp
+    app.register_blueprint(api_bp)
+
+    return app
 
 
 
