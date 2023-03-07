4ee7c76b03d544189a5f8faae6592ef5bb95b90f
Martin Ruthaivilavan <mmcruthai@gmail.com>
2023-03-07 09:01:27 -0600

    adjustment of button and route

--- app/blueprints/social/routes.py
+++ app/blueprints/social/routes.py
@@ -1,11 +1,11 @@
 from . import bp as social_bp
 from app.blueprints.social.models import User, Livestock
 from app.forms import LivestockForm, SearchForm
-from flask import render_template, redirect, flash, url_for
+from flask import render_template, redirect, flash, url_for, request
 from flask_login import login_required, current_user
 import requests
 
-user_search= []
+
 #route for social login
 @social_bp.route('/user/<username>')
 def user(username):
@@ -15,32 +15,31 @@ def user(username):
     nett = user_match.collections
     return render_template('user.jinja', user=user_match, nett=nett, title='Profile')  
 
-#route for collecting animals
+#Searchering for fish
 @social_bp.route('/collectanimals', methods=['GET', 'POST'])
 @login_required
 def collectanimals():
     form = SearchForm()
     search= form.search.data
     if form.validate_on_submit():
-        user_search.clear()
         col = requests.get(f'https://www.fishwatch.gov/api/species/{search}')
         data = col.json()
-        user_search.append(data[0]['Species Name'])
         return render_template('collectanimals.jinja',form=form, data=data, title='Collect Animals')
-    print(user_search)
     return render_template('collectanimals.jinja', form=form, title='Collect Animals')
 
+# Collecting fish
 @social_bp.route('/species', methods=['GET', 'POST'])
 def collected():
     # print('working??')
     form = SearchForm()
-    species= user_search[0]
+    species= request.form['name'] 
     colcard = Livestock(species=species, user_id=current_user.id)
     colcard.commit()
     # print('again?')
     return render_template('collectanimals.jinja', form=form, title='Collect Animals')
 
-@social_bp.route('/species/<species>,<username>', methods=['GET', 'POST'])
+#user seeing collections
+@social_bp.route('/species/<species>/<username>', methods=['GET', 'POST'])
 def seefish(species,username):
     user_match = User.query.filter_by(username=username).first()
     if not user_match:
@@ -50,8 +49,6 @@ def seefish(species,username):
     col = requests.get(f'https://www.fishwatch.gov/api/species/{species}')
     data = col.json()
     return render_template('user.jinja', data=data, user=user_match, nett=nett, title='Profile')
-
-
 
 @social_bp.route('/livestock', methods=['GET', 'POST'])
 @login_required
